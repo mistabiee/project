dff3b8f0010a3e44534f191fa3d5721f9ae42505
mistabiee <96842141+mistabiee@users.noreply.github.com>
2022-05-31 18:14:16 +0100

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,20 +1,14 @@
-#define a list of devices ( import from a text file | import from excel)
-#for each of the devices in the list
-    #login into the device
-    #enter username and password
-    #grab pre config changes (write to file)
-    #depoly config template (write to device)
-    #grab post config changes (write to file)
 import paramiko
 import os.path
 device_file = open("Device.txt").read().splitlines()
 cred = open("cred.txt").read().splitlines()
 un = cred[0]
 pwd =cred[1]
-for device in device_file:
-#For each device in device_file login
+def login():
    ssh = paramiko.SSHClient()
-   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())           #Add SSH host key automatically if required
+   ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Add SSH host key automatically if required
    ssh.connect(device, port=22, username=un, password=pwd)
    print("I'm in")
    ssh.close()
+for device in device_file:
+   login()
